6ada742c3288853a42341d4b76377eb1707a4a87
ebifurei <aldiansyahtrie@gmail.com>
2022-12-08 14:33:13 +0700

    Auth UI

--- dashboard/resources/js/Pages/Auth/Login.vue
+++ dashboard/resources/js/Pages/Auth/Login.vue
@@ -1,70 +1,82 @@
 <script setup>
-import Checkbox from '@/Components/Breeze/Checkbox.vue';
-import GuestLayout from '@/Layouts/Breeze/GuestLayout.vue';
-import InputError from '@/Components/Breeze/InputError.vue';
-import InputLabel from '@/Components/Breeze/InputLabel.vue';
-import PrimaryButton from '@/Components/Breeze/PrimaryButton.vue';
-import TextInput from '@/Components/Breeze/TextInput.vue';
-import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
+import { useForm, Head, Link } from '@inertiajs/inertia-vue3'
+import { mdiAccount, mdiAsterisk } from '@mdi/js'
+import LayoutGuest from '@/Layouts/LayoutGuest.vue'
+import SectionFullScreen from '@/Components/SectionFullScreen.vue'
+import CardBox from '@/Components/CardBox.vue'
+import FormCheckRadioGroup from '@/Components/FormCheckRadioGroup.vue'
+import FormField from '@/Components/FormField.vue'
+import FormControl from '@/Components/FormControl.vue'
+import BaseDivider from '@/Components/BaseDivider.vue'
+import BaseButton from '@/Components/BaseButton.vue'
+import BaseButtons from '@/Components/BaseButtons.vue'
+import FormValidationErrors from '@/Components/FormValidationErrors.vue'
+import NotificationBarInCard from '@/Components/NotificationBarInCard.vue'
+import BaseLevel from '@/Components/BaseLevel.vue'
 
-defineProps({
-    canResetPassword: Boolean,
-    status: String,
-});
+const props = defineProps({
+  canResetPassword: Boolean,
+  status: {
+    type: String,
+    default: null
+  }
+})
 
 const form = useForm({
-    email: '',
-    password: '',
-    remember: false
-});
+  email: '',
+  password: '',
+  remember: []
+})
 
 const submit = () => {
-    form.post(route('login'), {
-        onFinish: () => form.reset('password'),
-    });
-};
+  form
+    .transform(data => ({
+      ...data,
+      remember: form.remember && form.remember.length ? 'on' : ''
+    }))
+    .post(route('login'), {
+      onFinish: () => form.reset('password'),
+    })
+}
 </script>
 
 <template>
-    <GuestLayout>
-        <Head title="Log in" />
+  <LayoutGuest>
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
+    <Head title="Login" />
 
-        <form @submit.prevent="submit">
-          <div>
-            <InputLabel for="email" value="Email" />
-            <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus
-              autocomplete="username" />
-            <InputError class="mt-2" :message="form.errors.email" />
-          </div>
+    <SectionFullScreen v-slot="{ cardClass }" bg="purplePink">
+      <CardBox :class="cardClass" is-form @submit.prevent="submit">
+        <FormValidationErrors />
 
-          <div class="mt-4">
-            <InputLabel for="password" value="Password" />
-            <TextInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required
-              autocomplete="current-password" />
-            <InputError class="mt-2" :message="form.errors.password" />
-          </div>
+        <NotificationBarInCard v-if="status" color="info">
+          {{ status }}
+        </NotificationBarInCard>
 
-          <div class="block mt-4">
-            <label class="flex items-center">
-              <Checkbox name="remember" v-model:checked="form.remember" />
-              <span class="ml-2 text-sm text-gray-600">Remember me</span>
-            </label>
-          </div>
+        <FormField label="Email" label-for="email" help="Please enter your email">
+          <FormControl v-model="form.email" :icon="mdiAccount" id="email" autocomplete="email" type="email" required />
+        </FormField>
 
-          <div class="flex items-center justify-end mt-4">
-            <Link v-if="canResetPassword" :href="route('password.request')"
-              class="underline text-sm text-gray-600 hover:text-gray-900">
-            Forgot your password?
-            </Link>
+        <FormField label="Password" label-for="password" help="Please enter your password">
+          <FormControl v-model="form.password" :icon="mdiAsterisk" type="password" id="password"
+            autocomplete="current-password" required />
+        </FormField>
 
-            <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-              Log in
-            </PrimaryButton>
-          </div>
-        </form>
-    </GuestLayout>
+        <FormCheckRadioGroup v-model="form.remember" name="remember" :options="{ remember: 'Remember' }" />
+
+        <BaseDivider />
+
+        <BaseLevel>
+          <BaseButtons>
+            <BaseButton type="submit" color="info" label="Login" :class="{ 'opacity-25': form.processing }"
+              :disabled="form.processing" />
+            <BaseButton v-if="canResetPassword" route-name="password.request" color="info" outline label="Remind" />
+          </BaseButtons>
+          <Link :href="route('register')">
+          Register
+          </Link>
+        </BaseLevel>
+      </CardBox>
+    </SectionFullScreen>
+  </LayoutGuest>
 </template>
--- dashboard/resources/js/Pages/Auth/ResetPassword.vue
+++ dashboard/resources/js/Pages/Auth/ResetPassword.vue
@@ -1,60 +1,69 @@
 <script setup>
-import GuestLayout from '@/Layouts/Breeze/GuestLayout.vue';
-import InputError from '@/Components/Breeze/InputError.vue';
-import InputLabel from '@/Components/Breeze/InputLabel.vue';
-import PrimaryButton from '@/Components/Breeze/PrimaryButton.vue';
-import TextInput from '@/Components/Breeze/TextInput.vue';
-import { Head, useForm } from '@inertiajs/inertia-vue3';
+import { useForm, Head, Link } from '@inertiajs/inertia-vue3'
+import { mdiEmail, mdiFormTextboxPassword } from '@mdi/js'
+import LayoutGuest from '@/Layouts/LayoutGuest.vue'
+import SectionFullScreen from '@/Components/SectionFullScreen.vue'
+import CardBox from '@/Components/CardBox.vue'
+import FormField from '@/Components/FormField.vue'
+import FormControl from '@/Components/FormControl.vue'
+import BaseDivider from '@/Components/BaseDivider.vue'
+import BaseButton from '@/Components/BaseButton.vue'
+import FormValidationErrors from '@/Components/FormValidationErrors.vue'
 
 const props = defineProps({
-    email: String,
-    token: String,
-});
+  email: {
+    type: String,
+    default: null
+  },
+  token: {
+    type: String,
+    default: null
+  }
+})
 
 const form = useForm({
-    token: props.token,
-    email: props.email,
-    password: '',
-    password_confirmation: '',
-});
+  token: props.token,
+  email: props.email,
+  password: '',
+  password_confirmation: '',
+})
 
 const submit = () => {
-    form.post(route('password.update'), {
-        onFinish: () => form.reset('password', 'password_confirmation'),
-    });
-};
+  form
+    .post(route('password.update'), {
+      onFinish: () => form.reset('password', 'password_confirmation'),
+    })
+}
 </script>
 
 <template>
-    <GuestLayout>
-        <Head title="Reset Password" />
+  <LayoutGuest>
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
-                <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus autocomplete="username" />
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
+    <Head title="Reset Password" />
 
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-                <TextInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required
-                  autocomplete="new-password" />
-                <InputError class="mt-2" :message="form.errors.password" />
-                </div>
+    <SectionFullScreen v-slot="{ cardClass }" bg="purplePink">
+      <CardBox :class="cardClass" is-form @submit.prevent="submit">
+        <FormValidationErrors />
 
-                <div class="mt-4">
-                  <InputLabel for="password_confirmation" value="Confirm Password" />
-                  <TextInput id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation"
-                    required autocomplete="new-password" />
-                  <InputError class="mt-2" :message="form.errors.password_confirmation" />
-                </div>
+        <FormField label="Email" label-for="email" help="Please enter your email">
+          <FormControl v-model="form.email" :icon="mdiEmail" autocomplete="email" type="email" id="email" required />
+        </FormField>
 
-                <div class="flex items-center justify-end mt-4">
-                  <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Reset Password
-                  </PrimaryButton>
-                </div>
-                </form>
-    </GuestLayout>
+        <FormField label="Password" label-for="password" help="Please enter new password">
+          <FormControl v-model="form.password" :icon="mdiFormTextboxPassword" type="password"
+            autocomplete="new-password" id="password" required />
+        </FormField>
+
+        <FormField label="Confirm Password" label-for="password_confirmation" help="Please confirm new password">
+          <FormControl v-model="form.password_confirmation" :icon="mdiFormTextboxPassword" type="password"
+            autocomplete="new-password" id="password_confirmation" required />
+        </FormField>
+
+        <BaseDivider />
+
+        <BaseButton type="submit" color="info" label="Reset password" :class="{ 'opacity-25': form.processing }"
+          :disabled="form.processing" />
+      </CardBox>
+    </SectionFullScreen>
+  </LayoutGuest>
 </template>
