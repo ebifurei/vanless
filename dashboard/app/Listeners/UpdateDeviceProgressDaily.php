0be65f3adbec10d2532e599207650ad98922184b
ebifurei <aldiansyahtrie@gmail.com>
2022-12-23 14:52:14 +0700

    implement queue on listener

--- dashboard/app/Listeners/UpdateDeviceProgressDaily.php
+++ dashboard/app/Listeners/UpdateDeviceProgressDaily.php
@@ -7,7 +7,7 @@ use App\Models\UplinkCounterDaily;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
-class UpdateDeviceProgressDaily
+class UpdateDeviceProgressDaily implements ShouldQueue
 {
     /**
      * Create the event listener.
