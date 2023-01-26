ccbd2c30e16838ca0407030c936e70bd5f1439c4
ebifurei <aldiansyahtrie@gmail.com>
2023-01-26 10:53:32 +0700

    change downlink test

--- dashboard/tests/Feature/DownlinkTest.php
+++ dashboard/tests/Feature/DownlinkTest.php
@@ -2,13 +2,14 @@
 
 namespace Tests\Feature;
 
+use App\Models\Device;
 use App\Services\Chirpstack\ChirpStackDownlink;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use GuzzleHttp\Client;
 
-class ChirpStackDownlinkTest extends TestCase
+class DownlinkTest extends TestCase
 {
     protected $chirpStackDownlink;
 
@@ -28,5 +29,25 @@ class ChirpStackDownlinkTest extends TestCase
         $port = 1;
         $response = $this->chirpStackDownlink->sendDownlink($deviceEui, $payload, $port);
         $this->assertNotNull($response);
+    }
+
+
+    public function testSendDownlinkEndPoint()
+    {
+        $this->login();
+        $device = Device::factory()->create([
+            'device_id' => 'VanLess-1',
+            'device_eui' => '374d742c65b7e2ca'
+        ]);
+        $downlinkData = [
+            'device_id' => $device->device_id,
+            'payload' => 1,
+            'port' => 5,
+        ];
+
+        // send a request to the endpoint with downlink data
+        $response = $this->post(route('downlink.send'), $downlinkData);
+
+        $response->assertStatus(204);
     }
 }
